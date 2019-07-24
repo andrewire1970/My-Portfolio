493216c5e6399b7de38df870fc89cb1cb204ec68
Andrew <andrewor1994@outlook.ie>
2019-07-24 18:49:39 +0100

    changed images

--- js/main.js
+++ js/main.js
@@ -99,28 +99,3 @@ class TypeWriter {
     setTimeout(() => this.type(), typeSpeed);
   }
 }
-
-var i = 0; // Start point
-var images = [];
-var time = 4000;
-
-// Image List
-images[0] = "img/social1.jpg";
-images[1] = "img/social2.jpg";
-images[2] = "img/social1.jpg";
-images[3] = "img/social2.jpg";
-
-// Change Image
-function changeImg() {
-  document.slide.src = images[i];
-
-  if (i < images.length - 1) {
-    i++;
-  } else {
-    i = 0;
-  }
-
-  setTimeout("changeImg()", time);
-}
-
-window.onload = changeImg;
